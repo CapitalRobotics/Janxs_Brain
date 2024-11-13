32ea6268017d2c7de9477dd52f1633c4affbcd2a
Torin Etheridge <torinriley220@gmail.com>
2024-11-13 18:13:42 -0500

    I made the arm code less bad.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep24_25/auto/armMaybe2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep24_25/auto/armMaybe2.java
@@ -14,12 +14,12 @@ public class armMaybe2 extends LinearOpMode {
     // Position of the arm when it's lifted
     int armDownPosition = 150;
     // Find a motor in the hardware map named "Arm Motor"
-    DcMotorEx armMotor = hardwareMap.get(DcMotorEx.class, "arm");
-    Servo claw = hardwareMap.get(Servo.class, "bob");
+
 
     @Override
     public void runOpMode() throws InterruptedException {
-
+        DcMotorEx armMotor = hardwareMap.get(DcMotorEx.class, "arm");
+        Servo claw = hardwareMap.get(Servo.class, "claw");
 
         // Reset the motor encoder so that it reads zero ticks
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -31,6 +31,11 @@ public class armMaybe2 extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
+            if (gamepad2.left_trigger!=0) {
+                claw.setPosition(1);
+            } else if (gamepad2.right_trigger!=0) {
+                claw.setPosition(-1);
+            }
             // If the A button is pressed, raise the arm
             armMotor.setTargetPosition(getPosition());
             armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -56,21 +61,15 @@ public class armMaybe2 extends LinearOpMode {
         }
     }
     public void claw() {
-        if (gamepad2.left_bumper) {
-            claw.setPosition(1);
-        } else if (gamepad2.right_bumper) {
-            claw.setPosition(-1);
-        }
+
     }
         public int getPosition(){
             int pos = 0;
             if (gamepad2.a) {
-                pos = armUpPosition;
+                pos = armDownPosition;
             }
-
-            // If the B button is pressed, lower the arm
-            if (gamepad2.b) {
-               pos = armDownPosition;
+            else{
+                pos = armUpPosition;
             }
             return pos;
         }
