3819a710d64f154cbb4cf522e3c15f717c1f6e79
cheesebroccoli <chenchenmacc@gmail.com>
2025-03-07 17:05:17 -0500

    set position with new GoBILDA motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep24_25/teleop/arm75.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeep24_25/teleop/arm75.java
@@ -8,95 +8,91 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.IntoTheDeep24_25.pidMaybe;
 import org.firstinspires.ftc.teamcode.TemplateJanx;
 
-@Disabled
+
 @TeleOp(name = "hi")
 public class arm75 extends OpMode{
-    private static final int ARM_UP_POSITION = 67;
-    private static final int ARM_DOWN_POSITION = 15;
-    DcMotorEx arm,extender;
+    private static final int ARM_DOWN_POSITION = 0;
+    private static final int LEVEL_1 = 500;
+    private static final int LEVEL_2 = 3000;
+    DcMotorEx arm;
     // DcMotorEx frontLeft,frontRight,backLeft,backRight;
     TemplateJanx janx;
     Servo claw;
     Boolean pos = true;
+    pidMaybe pid;
+    ElapsedTime time;
+
     @Override
     public void init(){
-        janx = new TemplateJanx(hardwareMap);
-        janx.wheelInit("fr","br","bl","fl");
-        claw = hardwareMap.get(Servo.class,"claw");
-//        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//        arm.setTargetPosition(ARM_DOWN_POSITION);
-//        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm = hardwareMap.get(DcMotorEx.class,"bob");
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setTargetPosition(ARM_DOWN_POSITION);
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        time = new ElapsedTime();
     }
     @Override
     public void loop(){
-        claw();
-       // extend(gamepad2.right_stick_y);
-        //janx.drive(gamepad1.left_stick_x,gamepad1.left_stick_y,gamepad1.right_stick_x);
-        //arm.setPower(0);
-
-//        telemetry.addData("pos",arm.getCurrentPosition());
-//        telemetry.addData("pos",arm.getTargetPosition());
-        telemetry.update();
+        armTwo();
+        telemetry.addData("pos: ", arm.getCurrentPosition());
+        telemetry.addData("target: ", arm.getTargetPosition());
 
     }
-
-    public void extend(double x){
-        if ((Math.abs(x) > 0.3)) {
-            extender.setPower(x);
-        } else {
-            extender.setPower(0);
+    public void armTwo()
+    {
+        if(gamepad1.a)
+        {
+            arm.setTargetPosition(LEVEL_1);
         }
-    }
-
-    public void test(){
-        if(gamepad2.y){
-            claw.setPosition(1);
+        else if(gamepad1.b)
+        {
+            arm.setTargetPosition(LEVEL_2);
         }
-        else{
-            claw.setPosition(0);
-        }
-    }
-    public void claw(){
-        if (gamepad2.left_trigger!=0) {
-            claw.setPosition(1);
-        } else if (gamepad2.right_trigger!=0) {
-            claw.setPosition(-1);
-        }
-    }
-    public int getPosition(){
-        int pos = 0;
-        if (gamepad2.a) {
-            pos = ARM_DOWN_POSITION;
-        }
-        else{
-            pos = ARM_UP_POSITION;
-        }
-        return pos;
-    }
-    public void setArm(){
-        if(gamepad2.a){
-            arm.setTargetPosition(ARM_UP_POSITION);
-        }
-        else if(gamepad2.b){
+        else if(gamepad1.x)
+        {
             arm.setTargetPosition(ARM_DOWN_POSITION);
         }
-        else{
-            arm.setTargetPosition(arm.getCurrentPosition());
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm.setPower(1);
+    }
+    public void armTest()
+    {
+        if(Math.abs(gamepad1.left_stick_y)>0.1){
+            if(gamepad1.left_stick_y>0){
+                arm.setPower(1);
+            }
+            if(gamepad1.left_stick_y<0){
+                arm.setPower(-1);
+            }
         }
     }
-    public void armTestOne(){
-        if(gamepad2.a){
-            arm.setPower(1);
+    public void armOne()
+    {
+        pid = new pidMaybe(0.003,0.000,0.4);
+        double power = pid.calculatePower(positioning(gamepad2.a,gamepad2.b,gamepad2.x), arm.getCurrentPosition(),time.seconds());
+        arm.setTargetPosition(positioning(gamepad2.a,gamepad2.b,gamepad2.x));
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm.setPower(1);
+    }
+    public int positioning(boolean a, boolean b, boolean x)
+    {
+        int pos = 0;
+        if(a){
+            pos = ARM_DOWN_POSITION;
         }
-        else if (gamepad2.b) {
-            arm.setPower(-1);
+        else if(x){
+            pos = LEVEL_2;
         }
-        else{
-            arm.setPower(0);
+        else if(b)
+        {
+            pos = LEVEL_1;
         }
+        return pos;
     }
 }
